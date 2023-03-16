481533b2d2c49a6e2ddbb4d58e79e4a542b53f08
Retford <retfordjb@gmail.com>
2023-03-16 11:57:03 -0500

    clase03 completada

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const btnCreate = document.querySelector('[data-form-btn]');
 const createTask = (e) => {
   e.preventDefault();
   const inputTask = document.querySelector('[data-form-input]');
-  console.log(inputTask.value);
+  const inputValue = inputTask.value;
+  const listTask = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  inputTask.value = '';
+  const content = `
+  <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${inputValue}</span>
+  </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;
+  listTask.appendChild(task);
 };
 
 btnCreate.addEventListener('click', createTask);
